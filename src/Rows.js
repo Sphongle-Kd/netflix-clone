d7c0b921e3dc6712122ab159853cb8be8f355cba
sphongle <kamaldeopa56@gmail.com>
2022-07-06 22:58:28 +0530

    initial commit

---
+++ src/Rows.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axiosInstance";
+import "./Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original";
+
+function Rows({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+
+  // A snippet of code which runs on a specific condition/variable
+
+  useEffect(() => {
+    // if [],run once when the row loads, and dont run again
+
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      console.log("Response: ", request.data);
+      setMovies(request.data.results);
+      // return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  // console.table(movies);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row_posters">
+        {/* serval row_posters */}
+
+        {movies?.map((movie) => (
+          <img
+            key={movie.Id}
+            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default React.memo(Rows);
